8a63f886d911e4a3589ad5c10196feccd967ab34
João Pedro <jhonpitere163@gmail.com>
2025-03-31 20:54:10 -0300

    ok

--- Web/assets/js/scripts.js
+++ Web/assets/js/scripts.js
@@ -10,3 +10,27 @@ function toggleMenu() {
         menuList.style.display = "block";
     }
 }
+
+function copyCode(button) {
+    const targetId = button.dataset.target;
+    const codeBlock = document.getElementById(targetId).innerText;
+
+    navigator.clipboard.writeText(codeBlock).then(() => {
+        let toast = new bootstrap.Toast(document.getElementById('liveToast'));
+        toast.show();
+    }).catch(err => console.error("Erro ao copiar código:", err));
+}
+
+function toggleContent() {
+    let moreContent = document.getElementById("more-content");
+    let dots = document.getElementById("dots");
+
+    moreContent.classList.toggle("d-none");
+
+    // Oculta os "dots" quando o conteúdo for exibido
+    if (!moreContent.classList.contains("d-none")) {
+        dots.style.display = "none";
+    } else {
+        dots.style.display = "block";
+    }
+}
--- Web/src/js/pageLoad.js
+++ Web/src/js/pageLoad.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     function carregarConteudo(arquivo, container) {
         const caminho = `base/${arquivo}.html`; // Adiciona automaticamente 'base/' e '.html'
-        
+        const caminhoCode = `base/code/${arquivo}.html`
         fetch(caminho)
             .then(response => {
                 if (!response.ok) throw new Error(`Erro HTTP! Status: ${response.status}`);
@@ -16,13 +16,33 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })
             .catch(error => console.error(`Erro ao carregar ${caminho}:`, error));
+        fetch(caminhoCode)
+            .then(response => {
+                if (!response.ok) throw new Error(`Erro HTTP! Status: ${response.status}`);
+                return response.text();
+            })
+            .then(html => {
+                const elemento = document.getElementById(container);
+                if (elemento) {
+                    elemento.innerHTML = html;
+                } else {
+                    console.warn(`Elemento com ID "${container}" não encontrado.`);
+                }
+            })
+            .catch(error => console.error(`Erro ao carregar ${caminhoCode}:`, error));
     }
+
 
     const elementos = [
         { arquivo: 'navbar', container: 'container-navbar' },
         { arquivo: 'offcanvas_show', container: 'container-offcanvas_show' },
         { arquivo: 'conteudo_docs', container: 'container-conteudo_docs' },
+        { arquivo: 'code', container: 'container-conteudo_docs' },
     ]
-
+    const elementosCode = [
+        { arquivo: 'code1', container: 'container-code1' },
+        { arquivo: 'code2', container: 'container-code2' },
+    ]
     elementos.forEach(item => carregarConteudo(item.arquivo, item.container));
+    elementosCode.forEach(item => carregarConteudo(item.arquivo, item.container));
 });
